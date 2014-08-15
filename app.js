1d2df79dd0e5e483afd451a81ec7429a9903a307
rgrodin <grodin.robby@gmail.com>
2014-08-15 01:52:28 +0000

    Launching Splash Page

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express')
+  , routes = require('./routes')
+  , lists = require('./routes/lists')
+  , reports = require('./routes/reports')
+  , http = require('http')
+  , path = require('path')
+  , mcapi = require('./node_modules/mailchimp-api/mailchimp');
+
