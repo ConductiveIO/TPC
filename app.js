aa0535c3007dc56493c5a5b2584d906145a89c95
rgrodin <grodin.robby@gmail.com>
2014-08-15 02:11:32 +0000

    removed node code

--- app.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-  , routes = require('./routes')
-  , lists = require('./routes/lists')
-  , reports = require('./routes/reports')
-  , http = require('http')
-  , path = require('path')
-  , mcapi = require('./node_modules/mailchimp-api/mailchimp');
-
