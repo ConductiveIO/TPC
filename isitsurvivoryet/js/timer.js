9d287f408b6e2d6d7f44dabdbf2ea079ea080952
rgrodin <grodin.robby@gmail.com>
2015-01-28 21:36:47 +0000

    Removed unused files

--- isitsurvivoryet/js/timer.js
+++
@@ -1,5 +0,0 @@
-
-var d = new Date(2014, 9, 15, 20, 0, 0, 0);
-$('#countbox').countdown({until: d}, 'HM');
-
-
--- toypig/nav-fade.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(document).scrollTop() > 600) {
-            $(".navbar").addClass("scrolled");
-        } else {
-            $(".navbar").removeClass("scrolled");
-        }
-    });
-});
